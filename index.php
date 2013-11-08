e9aebd618d6cc624dbc6c4a4c178e1734830beb2
Oleg Kudrenko <oleg.kudrenko@gmail.com>
2013-11-08 16:20:10 +0200

    add index.php and structure

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    echo 'plese place here social icons of linkedin, fb, vk;'
+?>
+Make them centered (v and h)
+Use compas sprites for images and some styles with css3 like background gradient and box sizing.
